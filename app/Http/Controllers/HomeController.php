63dfae7fca17e63859c57537600df3f337be22aa
khatri344jeevan <jiban.khatri023@apexcollege.edu.np>
2025-06-25 20:43:03 +0545

    Initial Laravel setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        if (condition) {
+            # code...
+        }
+        return view('admin.index');
+    }
+
+}
