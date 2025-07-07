2857144e1396c83fea18ec2c7286ec9d14f7c789
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-07-07 23:45:07 +0545

    Dashboard user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('User Dashboard') }}
@@ -14,4 +14,15 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+
+</x-app-layout> --}}
+@extends('user.layout.sidebar')
+@section('title', 'My Dashboard')
+
+@section('header', 'User Dashboard')
+
+@section('content')
+    <div class="bg-white p-6 rounded shadow">
+        User is logged in
+    </div>
+@endsection
