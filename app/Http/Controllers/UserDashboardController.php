7c6a2b0b04bd86e9823a4410e9c1824d191104dd
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-07-09 00:20:54 +0545

    lauout structured, usercontroller made, proprty database,eloquent dile for property

---
+++ app/Http/Controllers/UserDashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserDashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        $propertyCount = $user->properties()->count();
+
+        return view('user.dashboard', compact('propertyCount'));
+            return 'User dashboard controller is working';
+
+    }
+}
