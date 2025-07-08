2ca1a1ab5356cd209a41e520079d0fc3583d68e5
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-07-08 21:57:42 +0545

    User layout modification and dashboard details

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,14 +15,55 @@
         </div>
     </div>
 
-</x-app-layout> --}}
+</x-app-layout>  --}}
 @extends('user.layout.sidebar')
 @section('title', 'My Dashboard')
 
 @section('header', 'User Dashboard')
 
 @section('content')
-    <div class="bg-white p-6 rounded shadow">
-        User is logged in
+    <section class="mt-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="bg-gray-100 rounded-lg p-4 border border-gray-200 shadow-md flex items-center justify-center">
+            <div class="flex items-center justify-center w-12 h-12 bg-pink-200 rounded-full mr-2">
+                <i class="bi bi-calendar-check text-3xl text-pink-600"></i>
+            </div>
+            <div>
+                <p class="text-gray-800 text-lg font-semibold">Total Bookings</p>
+                <p class="text-gray-600 text-sm">45</p>
+            </div>
+        </div>
+
+        <div class="bg-gray-100 rounded-lg p-4 border border-gray-200 shadow-md flex items-center justify-center">
+            <div class="flex items-center justify-center w-12 h-12 bg-red-200 rounded-full mr-2">
+                <i class="bi bi-house-door text-3xl text-red-600"></i>
+            </div>
+            <div>
+                <p class="text-gray-800 text-lg font-semibold">Total Properties</p>
+                <p class="text-gray-600 text-sm">28</p>
+            </div>
+        </div>
+
+        <div class="bg-gray-100 rounded-lg p-4 border border-gray-200 shadow-md flex items-center justify-center">
+            <div class="flex items-center justify-center w-12 h-12 bg-green-200 rounded-full mr-2">
+                <i class="bi bi-clock text-3xl text-green-600"></i>
+            </div>
+            <div>
+                <p class="text-gray-800 text-lg font-semibold">Pending Services</p>
+                <p class="text-gray-600 text-sm">12</p>
+            </div>
+        </div>
+
+        <div class="bg-gray-100 rounded-lg p-4 border border-gray-200 shadow-md flex items-center justify-center">
+            <div class="flex items-center justify-center w-12 h-12 bg-blue-200 rounded-full mr-2">
+                <i class="bi bi-alarm text-3xl text-blue-600"></i>
+            </div>
+            <div>
+                <p class="text-gray-800 text-lg font-semibold">Upcoming Services</p>
+                <p class="text-gray-600 text-sm">5</p>
+            </div>
+        </div>
     </div>
+</section>
+
 @endsection
