7c8d040a880839e09b045dfc4dcd556ad6bf0dd0
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-07-08 21:57:42 +0545

    User layout modification and dashboard details

--- resources/views/user/layout/sidebar.blade.php
+++ resources/views/user/layout/sidebar.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>@yield('title', 'GharSewa Dashboard')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
-</head>
+@include('user.layout.head');
 
 <body class="bg-gray-100 flex font-sans">
     <!-- Sidebar -->
@@ -21,7 +12,7 @@
         <nav class="flex-1 overflow-y-auto mt-6">
             <ul class="space-y-3 px-6">
                 <li>
-                    <a href="/dashboard"
+                    <a href="../dashboard/index"
                         class="flex items-center gap-4 px-5 py-3 rounded-lg hover:bg-gray-400 hover:text-gray-900 transition font-semibold">
                         <i class="bi bi-speedometer2 text-lg"></i>
                         Dashboard
@@ -94,19 +85,11 @@
 
     <!-- Main Content -->
     <div class="flex-1 ml-64 min-h-screen flex flex-col bg-gray-70 mt-10 ">
-        <!-- Top bar -->
-        <header
-            class="fixed top-0 left-0 right-0 bg-gray-300 shadow-md py-6 px-4 pl-64 flex justify-between items-center border-b border-gray-400 z-20">
-            {{-- <h1 class="text-2xl font-extrabold text-gray-800">Dashboard</h1> --}}
-            <div class="ml-5 text-gray-700 text-base font-semibold ">
-                Welcome,
-                <span class="text-gray-900">{{ Auth::user()->name }}</span>
-            </div>
-        </header>
+       @include('user.layout.header')
 
 
         <!-- Page Content -->
-        <main class="flex-1 p-8">
+        <main class="flex-1 p-2">
             @yield('content')
         </main>
     </div>
