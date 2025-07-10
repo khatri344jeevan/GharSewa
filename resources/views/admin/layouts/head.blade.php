17e33d448f20194d6e9bd35a9218a8afd2b539a5
khatri344jeevan <jiban.khatri023@apexcollege.edu.np>
2025-07-10 18:18:13 +0545

    admin bkp

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -6,5 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>@yield('title', 'GharSewa Dashboard')</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
-</head> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
+</head>
