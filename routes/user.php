e9be0e031013f9144e7fdc3e0b6922196de33974
khatri344jeevan <jiban.khatri023@apexcollege.edu.np>
2025-07-19 12:55:19 +0545

    routing problem fixed with user

--- routes/user.php
+++ routes/user.php
@@ -1,6 +1,18 @@
 <?php
 use App\Http\Controllers\UserDashboardController;
 
-Route::get('/dashboard', [UserDashboardController::class, 'index'])
-    ->middleware(['auth', 'verified', 'rolemanager:user'])
-    ->name('user.dashboard');
+
+// Route::get('/dashboard', [UserDashboardController::class, 'index'])
+//     ->middleware(['auth', 'verified', 'rolemanager:user'])
+//     ->name('user.dashboard');
+
+Route::middleware(['auth', 'verified', 'rolemanager:user'])->group(function () {
+    Route::controller(UserDashboardController::class)->group(function () {
+        Route::prefix('user')->group(function () {
+            Route::get('/dashboard', 'index')->name('user.dashboard');
+            // Route::get('/profile', 'profile')->name('user.profile');
+
+
+        });
+    });
+});
