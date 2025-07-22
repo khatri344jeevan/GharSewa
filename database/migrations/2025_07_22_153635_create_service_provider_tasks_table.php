1eacecdfa976c3a84c93f66ae06796045dfbd834
Yunay <mandipjoshi21@gmail.com>
2025-07-22 21:50:44 +0545

    create new migration called service provider tasks table for dashboard

---
+++ database/migrations/2025_07_22_153635_create_service_provider_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_provider_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_provider_id'); //unsignedBigInteger - only takes positive values
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('service_provider_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_provider_tasks');
+    }
+};
