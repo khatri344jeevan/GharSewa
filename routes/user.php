fc3419a6b73d3362bbd2668d4ceeeb4ea719f68f
Yunay <mandipjoshi21@gmail.com>
2025-07-22 11:10:42 +0545

    only for backup

--- routes/user.php
+++ routes/user.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\UserDashboardController;
 
 
@@ -6,17 +7,17 @@ Route::get('/dashboard', [UserDashboardController::class, 'index'])
     ->middleware(['auth', 'verified', 'rolemanager:user'])
     ->name('user.dashboard');
 
-Route::get('/properties',[UserDashboardController:: class, 'p_index'])
- ->name('user.Properties.p_index');
+Route::get('/properties', [UserDashboardController::class, 'p_index'])
+    ->name('user.Properties.p_index');
 
- Route::get('/properties/create',[UserDashboardController:: class, 'p_create'])
- ->name('user.Properties.p_create');
+Route::get('/properties/create', [UserDashboardController::class, 'p_create'])
+    ->name('user.Properties.p_create');
 
- Route::get('/properties/edit',[UserDashboardController:: class, 'p_edit'])
- ->name('user.Properties.p_edit');
+Route::get('/properties/edit', [UserDashboardController::class, 'p_edit'])
+    ->name('user.Properties.p_edit');
 
- Route::get('/properties/update',[UserDashboardController:: class, 'p_update'])
- ->name('user.Properties.p_update');
+Route::get('/properties/update', [UserDashboardController::class, 'p_update'])
+    ->name('user.Properties.p_update');
 
- Route::post('/properties/store', [UserDashboardController::class, 'p_store'])
-->name('user.Properties.p_store');
+Route::post('/properties/store', [UserDashboardController::class, 'p_store'])
+    ->name('user.Properties.p_store');
