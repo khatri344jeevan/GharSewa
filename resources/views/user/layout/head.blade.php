0ad9e24bf097d27065473b4398c0896a89018ff7
AabhashSharma <aavash1234567@gmail.com>
2025-07-22 14:10:30 +0545

    Style and Layout Modification

--- resources/views/user/layout/head.blade.php
+++ resources/views/user/layout/head.blade.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>@yield('title', 'GharSewa Dashboard')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+     <link rel="icon" href="{{ asset('images/Gharsewa.jpg') }}" type="image/jpeg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
 </head>
