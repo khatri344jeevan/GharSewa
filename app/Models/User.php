325201f5dc98f3a15c48086d1da3f3635660e91b
MandipJoshii <mandip.joshi824@gmail.com>
2025-07-22 10:33:38 +0545

    errors solving with route

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,36 +1,79 @@
+
 <?php
+
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'role', 'phone', 'address', 'password'];
-    protected $hidden = ['password', 'remember_token'];
-    protected $casts = ['email_verified_at' => 'datetime', 'password' => 'hashed'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'password',
+    ];
 
-    // THIS IS THE RELATIONSHIP FOR THE SERVICE PROVIDER DASHBOARD
-    public function serviceProviderProfile(): HasOne
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function serviceProvider()
     {
         return $this->hasOne(ServiceProvider::class);
     }
-    
-    // THESE ARE YOUR TEAM'S OTHER RELATIONSHIPS - THEY MUST EXIST
+
+    //properties eloquent
     public function properties(): HasMany
     {
         return $this->hasMany(Property::class);
     }
+
+    //booking eleoquent: One user can have multiple bookings
     public function bookings(): HasMany
     {
         return $this->hasMany(Booking::class);
     }
-    public function payments(): HasMany
+
+    //for counting the task
+    // public function tasks(){
+    //        return $this->hasMany(Task::class);
+    // }
+
+    public function payments()
     {
         return $this->hasMany(Payment::class);
     }
--- routes/service_provider.php
+++ routes/service_provider.php
@@ -1,18 +1,15 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceProvider\DashboardController;
-use App\Http\Controllers\ServiceProvider\ProfileController;
-use App\Http\Controllers\ServiceProvider\TaskController;
+
+Route::get('service-provider',  function(){
+    return view('service_provider.layouts.sidenav');
+});
 
 Route::middleware(['auth', 'verified', 'rolemanager:service_provider'])
     ->prefix('service-provider')
-    ->name('service-provider.')
     ->group(function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::put('/password', [ProfileController::class, 'updatePassword'])->name('password.update');
-    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-});
+        Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->name('service_provider.dashboard');
+    });
