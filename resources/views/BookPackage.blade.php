b28fa1f75c1538cc59ea1c549d44b93a71a33d96
AabhashSharma <aavash1234567@gmail.com>
2025-07-25 17:32:17 +0545

    for toaster message

--- resources/views/BookPackage.blade.php
+++ resources/views/BookPackage.blade.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Book Maintenance Package</title>
-    <link rel="icon" href="{{ asset('images/Gharsewa.jpg') }}" type="image/jpeg" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="icon" href="{{ asset('images/Gharsewa.jpg') }}" type="image/jpeg" />
 </head>
 
 <body>
